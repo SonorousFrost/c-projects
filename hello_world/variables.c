e9d6a243150ebe3210b95dcf8405e3f952137f09
Jessi Bevill <sonorousfrost@gmail.com>
2022-12-09 15:44:59 -0600

    Createfile and function

---
+++ hello_world/variables.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+    int age = 0;
+    // variables can be redefined with = operator 
+    age = 37.2;
+    printf("%u", age);
+    return 0;
+}
+
+// C built-in data types are as follows !! For char-type variables !!
+
+/* int char, short, long, float, double, long, double */
+
+// C built-in data types for Integers are 
+
+/* char, int, short and long */
+
+// int is often used to store integers, while char is more often used to store letters but can also hold integers from -128 to 127
+
+// for all above data types we can prepend 'unsigned' to start the range at 0 instead of a negative number.
+
+char secondary(void) {
+    unsigned char j;
+    return 0;
+}
